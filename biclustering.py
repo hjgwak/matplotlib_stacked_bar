55b80540d540a28f39cb0616898984e0c43ebeee
정지원 <jiwon@jeongjiwonui-MacBook-Air.local>
2017-04-02 18:59:31 +0900

    pvalue label 위치 제대로!

--- biclustering.py
+++ biclustering.py
@@ -6,41 +6,43 @@ import numpy as np
 from matplotlib import pyplot as plt
 from loadData import load_groupData
 
-def biclustering(all_data) :
+def biclustering(filtered, checked) :
 
-	n_clusters = (2, 2)
+	### over 2 
+	if len(filtered) >2 :
+		n_clusters = (2, 2)
+	else :
+		n_clusters = (1, 1)
 
 	model = SpectralBiclustering(n_clusters=n_clusters, method='log', random_state=0)
-	data = np.asarray(all_data['data'])
+	data = np.asarray(filtered['data'])
 	model.fit(data)
 
-	d1 = bd.draw_graph(group2, group1)
+	d1 = bd.draw_graph(group1, group2, checked)
 	#biclustering
 	y_fit_data = data[np.argsort(model.row_labels_)]
 	d1.fit_data = y_fit_data[:, np.argsort(model.column_labels_)]
-
-	d1.title = "After biclustering; rearranged to show biclusters"
 
 	#set x label
 	d1.x = np.argsort(model.column_labels_)
 	d1.x_label = [0 for i in range(len(d1.x))]
 	for n in range(len(d1.x)) :
 		d1.x_label[d1.x[n]] = n
-	d1.genus_data = all_data['genus']
 	d1.y_label = [i for i in np.argsort(model.row_labels_)]
-	d1.pvalue_label = all_data['pvalue']
+	d1.genus_data = filtered['genus']
+	d1.pvalue_label = filtered['pvalue']
+	d1.title = "After biclustering"
 	d1.draw()
 
 	# biclustering of fixed x-axis domain 
-	d2 = bd.draw_graph(group2,group1)
-	d2.x_label = [i for i in range(len(group2+group1))]
+	d2 = bd.draw_graph(group1, group2, checked)
+	d2.x_label = [i for i in range(len(group1+group2))]
 	d2.y_label = [i for i in np.argsort(model.row_labels_)]
-	d2.pvalue_label = all_data['pvalue']
-	d2.fit_data = y_fit_data
-	d2.genus_data = all_data['genus']
 	d2.x = d2.x_label
+	d2.fit_data = y_fit_data
+	d2.genus_data = filtered['genus']
+	d2.pvalue_label = filtered['pvalue']
 	d2.title = "After biclustering; fixed x domins "
-	
 	d2.draw()
 
 
@@ -59,32 +61,29 @@ def run(filename, group_filename, checked) :
 	group1 = []
 	group2 = [] 
 
-	#separate group1 and group2 
-	for n in range(len(group)) :
-		group1_i = group_names.index(checked[0])
-		group2_i = group_names.index(checked[1])
-
-		if group.values()[n] == group1_i :
-			group1.append(group.keys()[n])
-		elif group.values()[n] == group2_i :
-			group2.append(group.keys()[n])
-
-	group1_ind = []
+	#index of group in x_data
+	group1_ind = [] 
 	group2_ind = []
 	
-	#sort by order of control or case 
-	for n in range(len(x_data)-1) :
-		if n < len(group2) and x_data.count(group2[n]) == 1 :
-			group2_ind.append(x_data.index(group2[n]))
-		if n < len(group1) and x_data.count(group1[n]) == 1 :
-			group1_ind.append(x_data.index(group1[n]))
+	#separate group1 and group2 
+	for n in range(len(group)) :
+		group1_id = group_names.index(checked[0])
+		group2_id = group_names.index(checked[1])
 
-	all_data = {}
-	all_data['data'] = []
-	all_data['genus'] = []
-	all_data['pvalue'] = []
+		if group.values()[n] == group1_id :
+			group1.append(group.keys()[n])
+			group1_ind.append(x_data.index(group.keys()[n]))
+		elif group.values()[n] == group2_id :
+			group2.append(group.keys()[n])
+			group2_ind.append(x_data.index(group.keys()[n]))
 
-	d3 = bd.draw_graph(group2, group1)
+	filtered = {}
+	filtered['data'] = []
+	filtered['genus'] = []
+	filtered['pvalue'] = []
+
+	#it should be changed !! 
+	d3 = bd.draw_graph(group1, group2, checked)
 
 	#extract data which have low pvalue
 	for row_num in range(1,nrows):
@@ -95,33 +94,32 @@ def run(filename, group_filename, checked) :
 		test_group1 = []
 		test_group2 = []
 		
-		for coni in group2_ind :
-			test_group2.append(csv_list[row_num][2+coni])
-		for casi in group1_ind :
-			test_group1.append(csv_list[row_num][2+casi])
+		for i in group1_ind :
+			test_group1.append(csv_list[row_num][2+i])
+		for i in group2_ind :
+			test_group2.append(csv_list[row_num][2+i])
 
 		# calculate pvalue
-		pvalue = sci.ttest_ind(test_group2, test_group1, equal_var=True)[1]
+		pvalue = sci.ttest_ind(test_group1, test_group2, equal_var=True)[1]
 
 		if pvalue < 0.05 :
-			all_data['data'].append(test_group2+test_group1)
-			all_data['genus'].append(current_genus+"("+str(current_type)[0]+")")
-			all_data['pvalue'].append(round(pvalue,4))
+			filtered['data'].append(test_group1+test_group2)
+			filtered['genus'].append(current_genus+"("+str(current_type)[0]+")")
+			filtered['pvalue'].append(round(pvalue,4))
 
 
-	d3.x_label = [i for i in range(len(group2+group1))]
-	d3.y_label = [i for i in range(len(all_data['data']))]
-	d3.pvalue_label = all_data['pvalue']
-
-	d3.fit_data = all_data['data']
-	d3.genus_data = all_data['genus']
+	d3.x_label = [i for i in range(len(group1+group2))]
+	d3.y_label = [i for i in range(len(filtered['data']))]
+	d3.pvalue_label = filtered['pvalue']
+	d3.fit_data = filtered['data']
+	d3.genus_data = filtered['genus']
 	d3.title = "Original dataset"
 	d3.x = d3.x_label
 
 	d3.draw()
 
-	if len(all_data) > 0 :
-		biclustering(all_data)
+	if len(filtered) > 0 :
+		biclustering(filtered, checked)
 	else :
 		print "no data"
 
@@ -130,7 +128,7 @@ def run(filename, group_filename, checked) :
 if __name__ == "__main__":
 	filename = './data/Total_CRS_filtered.csv'
 	group_filename = './data/group_name.csv'
-	checked = ['case','control']
+	checked = ['case','etc']
 	run(filename, group_filename, checked)
 
 
--- biclustering_draw.py
+++ biclustering_draw.py
@@ -3,8 +3,8 @@ import matplotlib.lines as mlines
 
 class draw_graph(object):
 	"""docstring for draw_graph"""
-	control = []
-	case = []
+	group1 = []
+	group2 = []
 	x_label = []
 	y_label = []
 	pvalue_label = []
@@ -13,52 +13,60 @@ class draw_graph(object):
 	title = []
 	x = []
 
+	checked = []
 
-	def __init__(self, control, case):
+	def __init__(self, group1, group2, checked):
 		super(draw_graph, self).__init__()
-		self.control = control
-		self.case = case
+		self.group1 = group1
+		self.group2 = group2
+		self.checked = checked
 
 	def draw(self) :
-		f, ax1 = plt.subplots(1, figsize=(14,6), dpi=80)
-
-		plt.subplots_adjust(left=None, bottom=None, right=0.95, top=None,
+		f, ax1 = plt.subplots(1)
+		# f, ax1 = plt.subplots(1, figsize=(14,6), dpi=80)
+		plt.subplots_adjust(left=0.2, bottom=None, right=0.9, top=None,
 		                    wspace=None, hspace=None)
 
-		im = ax1.matshow(self.fit_data, cmap=plt.cm.Blues)
-		# plt.colorbar(im, orientation='horizontal')
+		im = ax1.imshow(self.fit_data, cmap=plt.cm.Blues)
+		ax1.set_aspect('auto')
+		plt.colorbar(im, orientation='horizontal')
 
 		plt.title(self.title)
 		
 		ax1.set_xticks(self.x_label)
-		ax1.set_xticklabels(self.control+self.case, fontsize=7, rotation=70)
+		ax1.set_xticklabels(self.group1+self.group2, fontsize=7, rotation=70)
 		
 		#set label color
 		colors = []
 		for n in self.x_label :
-			if self.x[n] >= len(self.control) :
-				colors.append('red')
-			else:
+			if self.x[n] < len(self.group1) :
 				colors.append('black')
+			else:
+				colors.append('red')
 		
+		#set the axis label color
 		for color,tick in zip(colors,ax1.xaxis.get_major_ticks()):
-			tick.label1.set_color(color) #set the color property
-
-
-		ax1.xaxis.set_ticks_position('bottom')
+			tick.label1.set_color(color) 
 
 		#y axis on left
-		ax1.set_yticks(self.y_label)
-		ax1.set_yticklabels(self.genus_data)
-		ax2 = ax1.twinx()
+		ax1.set_yticks(self.y_label, minor=False)
+		ax1.set_yticklabels(self.genus_data, fontsize = 7, minor=False)
+		ax1.tick_params(labelbottom='on',labeltop='off', labelleft="on", 
+    		top='off', left='off', right='off')
 		
+		# fig = plt.gcf()
+		# size = fig.get_size_inches()*fig.dpi
+		# print size
 		#y axis on right
-		y_data_2 = [len(self.pvalue_label)-n-1 for n in self.y_label]
-		ax2.set_yticks(y_data_2)
-		ax2.set_yticklabels(self.pvalue_label)
+		ax2 = f.add_axes(ax1.get_position(), frameon=False)
+		ax2.set_yticks(self.y_label, minor=False)
+		ax2.set_ylim(ax1.get_ylim())
+		ax2.set_yticklabels(self.pvalue_label, fontsize = 7, minor=False)
+		ax2.tick_params(labelbottom='off',labeltop='off', labelleft="off", labelright='on',
+    			bottom='off', left='off', right='off')
 
 		#set legend
-		black_line = mlines.Line2D([], [], color='black',label='control')
-		red_line = mlines.Line2D([], [], color='red',label='case')
-		plt.legend(handles=[black_line, red_line],bbox_to_anchor=(1, 1.13), loc='upper right', borderaxespad=0.)
+		black_line = mlines.Line2D([], [], color='black',label=self.checked[0])
+		red_line = mlines.Line2D([], [], color='red',label=self.checked[1])
+		plt.legend(handles=[black_line, red_line],loc='lower right', bbox_to_anchor=(1., 1.), fontsize =7)
 
